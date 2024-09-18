da5273163bb02d96ece961311440a09862a29fdc
cnobert <cnobert@nait.ca>
2024-09-18 09:32:43 -0600

    2024-09-18 end of class

--- Lesson05-Arithmetic-Exercises/obj/Debug/net8.0/Lesson05-Arithmetic-Exercises.AssemblyInfo.cs
+++ Lesson05-Arithmetic-Exercises/obj/Debug/net8.0/Lesson05-Arithmetic-Exercises.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lesson05-Arithmetic-Exercises")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af1deafd8bf832c2985fdd94e65b635f2555c2e7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a75e3bc3120e3e6df43a76455bb521475829c69c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lesson05-Arithmetic-Exercises")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lesson05-Arithmetic-Exercises")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
