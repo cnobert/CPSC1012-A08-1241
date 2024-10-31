0083f34a67cc469cc4e2bd8463cfb60adea33545
cnobert <cnobert@nait.ca>
2024-10-31 11:37:59 -0600

    end of class

--- Lesson16-Arrays02/obj/Debug/net8.0/Lesson16-Arrays02.AssemblyInfo.cs
+++ Lesson16-Arrays02/obj/Debug/net8.0/Lesson16-Arrays02.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lesson16-Arrays02")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7cd6fd6dad35cdef748fe5798aa7cf78666d44d5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f76abcd3b81316a21157e1c5d6af6874890fbfad")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lesson16-Arrays02")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lesson16-Arrays02")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Lesson20-FileIO-02/Program.cs
@@ -0,0 +1,86 @@
+﻿ 
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+        
+        const int ArrayLength = 5;
+        string fileName = "studentData.txt";
+        //in the assignment this would be daysOfMonth
+        string[] studentNames = new string[ArrayLength]; 
+        //in the assignment this would be salesForDay
+        int[] studentMarks = new int[ArrayLength];
+        
+        //loading logicalSize, studentNames, and studentMarks with test data
+        int logicalSize = 3;
+        // studentNames[0] = "Conrad Nobert";
+        // studentNames[1] = "Taylor Fast";
+        // studentNames[2] = "Brinda Kaur";
+
+        // studentMarks[0] = 30;
+        // studentMarks[1] = 98;
+        // studentMarks[2] = 79;
+        //FileWrite(studentNames, studentMarks, logicalSize, fileName);
+        logicalSize = FileRead(studentNames, studentMarks, fileName);
+        for(int c = 0; c < logicalSize; c++)
+        {
+            Console.WriteLine($"{studentNames[c]} {studentMarks[c]}");
+        }
+
+    }
+    static int FileRead(string[] studentNames, int[] studentMarks, string fileName)
+    {
+        int logicalSize = 0;
+        Console.WriteLine("-----Reading from file-----");
+
+        StreamReader reader = null;
+        if(File.Exists(fileName))
+        {
+            try
+            {
+                reader = new StreamReader(fileName);
+
+                //priming read
+                reader.ReadLine();
+                while(!reader.EndOfStream)
+                {
+                    string line = reader.ReadLine();
+                    //"Conrad Nobert, 30"
+                    studentNames[logicalSize] = line.Substring(0, line.IndexOf(","));
+                    string restOfLine = line.Substring(line.IndexOf(",") + 1);
+                    studentMarks[logicalSize] = int.Parse(restOfLine);
+                    logicalSize++;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Something went wrong: {e.Message}");
+            }
+        }
+
+        Console.WriteLine("-----File read!-----");
+        return logicalSize;
+    }
+    static void FileWrite(string[] studentNames, int[] studentMarks, int logicalLength, string fileName)
+    {
+        StreamWriter writer = null;
+        Console.WriteLine("-----Writing to file-----");
+        try
+        {
+            writer = new StreamWriter(fileName);
+            writer.WriteLine("Name, Mark");
+            for(int c = 0; c < logicalLength; c++)
+            {
+                writer.WriteLine($"{studentNames[c]},{studentMarks[c]}");
+            }
+            writer.Close();
+        }
+        catch(Exception e)
+        {
+            Console.WriteLine($"Something went wrong: {e.Message}");
+        }
+        Console.WriteLine("-----File written!-----");
+    }
+
+}
